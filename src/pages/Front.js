4cbe0f73097319415fdc9b41b21c475a6b93f37d
getafemean <getafemean@gmail.com>
2021-07-19 18:18:02 +0200

    styles

--- src/pages/Front.js
+++ src/pages/Front.js
@@ -3,16 +3,46 @@ import { getAllArticles } from '../services/ArticlesService';
 
 export default class Front extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            articles: []
+        }
+    }
 
     componentDidMount() {
         getAllArticles()
-            .then(res => console.log(res))
+            .then(res => {
+                this.setState({
+                    articles: res
+                })
+            })
+    }
+
+    articlesCards = () => {
+        const cards = this.state.articles.map((article, index) => {
+            return  <div className="card" key={index}>
+                        <div className="header-card">
+                            <h1>{article.brand}</h1>
+                        </div>
+                        <div className="body-card">
+                            <p>{article.model}</p>
+                            <p>{article.description}</p>
+                            <img src={article.pic} />
+                            <p>
+                                <span>{article.price} €</span>
+                                <span>{article.currentPrice} €</span>
+                            </p>
+                        </div>
+                    </div>
+        })
+        return cards;
     }
 
     render() {
         return (
-            <div>
-                
+            <div className="row grid">
+                {this.articlesCards()}
             </div>
         )
     }
